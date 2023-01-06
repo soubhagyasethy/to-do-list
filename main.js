1bc4d11c67decb3371d8a53ac14df3f0b922c766
Soubhagya Sethy <ssethy017@gmail.com>
2023-01-06 21:02:57 +0530

    feat: edit button added

--- main.js
+++ main.js
@@ -6,28 +6,30 @@ addButton.addEventListener("click", () => {
     let input = myInput.value;
 
     // creating task list
-    let task = document.createElement("p");
+    let task = document.createElement("div");
+    task.classList.add("task-style");
     toDo.appendChild(task);
-    task.classList.add("task-style")
-
+    
     task.innerText = myInput.value;
     myInput.value = "";
-
-    // creating a delete button
-    let deleteButton = document.createElement("button");
-    deleteButton.classList.add("deleteButton-style");
-    task.appendChild(deleteButton);
-    deleteButton.innerText = "X";
 
     // creating a edit button
     let editButton = document.createElement("button");
     editButton.classList.add("editButton-style");
-    task.appendChild(editButton);
+    toDo.appendChild(editButton);
     editButton.innerText = "edit";
+
+    // creating a delete button
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("deleteButton-style");
+    toDo.appendChild(deleteButton);
+    deleteButton.innerText = "X";
 
     // delete the task
     deleteButton.addEventListener("click", () => {
-        toDo.removeChild(task)
+        toDo.removeChild(task);
+        toDo.removeChild(editButton);
+        toDo.removeChild(deleteButton);
     })
 
     task.addEventListener("click", () => {
