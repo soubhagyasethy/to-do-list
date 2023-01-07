c64b1b9b5bcc20f476821c6236e1ff20e6136701
Soubhagya Sethy <ssethy017@gmail.com>
2023-01-08 01:27:09 +0530

    feat: added update button

--- main.js
+++ main.js
@@ -1,58 +1,76 @@
 let myInput = document.querySelector("#myInput");
 let addButton = document.querySelector("#addButton");
-let toDo = document.querySelector("#toDo");
+var toDo = document.querySelector("#toDo");
 
 addButton.addEventListener("click", () => {
     let input = myInput.value;
 
-    if (!input) {
+    if (input === "") {
         alert("Can't add empty task. Please write your task first!")
     }else {
         // creating toDoTask div 
-    let toDoTask = document.createElement("div");
-    toDoTask.classList.add("toDo-style");
-    toDo.appendChild(toDoTask);
+        let toDoTask = document.createElement("div");
+        toDoTask.classList.add("toDo-style");
+        toDo.appendChild(toDoTask);
 
-    // creating task list
-    let task = document.createElement("div");
-    task.classList.add("task-style");
-    toDoTask.appendChild(task);
+        // creating task list
+        let task = document.createElement("li");
+        task.classList.add("task-style");
+        toDoTask.appendChild(task);
 
-    task.innerText = myInput.value;
-    myInput.value = "";
+        task.innerText = myInput.value;
+        myInput.value = "";
 
-    // creating a edit button
-    let editButton = document.createElement("button");
-    editButton.classList.add("editButton-style");
-    toDoTask.appendChild(editButton);
-    editButton.innerText = "edit";
+        // creating a update button
+        let updateButton = document.createElement("button");
+        updateButton.classList.add("updateButton-style");
+        toDoTask.appendChild(updateButton);
+        updateButton.innerText = "update";
+        updateButton.style.display = "none";
 
-    // creating a delete button
-    let deleteButton = document.createElement("button");
-    deleteButton.classList.add("deleteButton-style");
-    toDoTask.appendChild(deleteButton);
-    deleteButton.innerText = "X";
+        // creating a edit button
+        let editButton = document.createElement("button");
+        editButton.classList.add("editButton-style");
+        toDoTask.appendChild(editButton);
+        editButton.innerText = "edit";
 
-    // edit the task
-    editButton.addEventListener("click", () => {
-        task.style.display = "none";
-        let edit_input = document.createElement("input");
-        edit_input.classList.add("edit-input-style");
-        toDoTask.appendChild(edit_input);
-        editButton.innerText = "save";
-    })
+        // creating a delete button
+        let deleteButton = document.createElement("button");
+        deleteButton.classList.add("deleteButton-style");
+        toDoTask.appendChild(deleteButton);
+        deleteButton.innerText = "X";
 
-    // delete the task
-    deleteButton.addEventListener("click", () => {
-        toDoTask.removeChild(task);
-        toDoTask.removeChild(editButton);
-        toDoTask.removeChild(deleteButton);
-    })
+        // selecting the task to edit it
+        let editInput = document.querySelector(".task-style");
+        editInput.contentEditable = false;
 
-    task.addEventListener("click", () => {
-        task.style.textDecoration = "line-through";
+        // edit the task
+        editButton.addEventListener("click", () => {
+            // let editInput = document.querySelector(".task-style");
+            editInput.contentEditable = true;
+            editInput.style.backgroundColor = "white"
+            editInput.style.border = "0.1rem solid grey";
+            updateButton.style.display = "";
+            editButton.style.display = "none";
         })
-    }
 
-})
+        // update the task
+        updateButton.addEventListener("click",() => {
+            // let editInput = document.querySelector(".task-style");
+            editInput.contentEditable = false;
+            editInput.style.backgroundColor = "rgb(216, 213, 213)"
+            editInput.style.border = "none";
+            updateButton.style.display = "none";
+            editButton.style.display = "";
+        })
+
+        // delete the task
+        deleteButton.addEventListener("click", () => {
+            toDoTask.removeChild(task);
+            toDoTask.removeChild(editButton);
+            toDoTask.removeChild(deleteButton);
+        })
+
+    }
+});
 
